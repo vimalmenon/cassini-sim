2dc272bb147d9e45428b716f3c0ecb85030e0102
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2020-10-24 15:25:51 +0800

    Basic header structure

--- scripts/page/header/index.tsx
+++ scripts/page/header/index.tsx
@@ -13,42 +13,65 @@ const StyledHeader = styled.header`
 	background: #272c4d;
 	border:1px solid #272c4d;
 	color:white;
+	align-items:center;
 	.profile {
 		display:flex;
+		max-height:3.6rem;
+		padding:0 0.843rem;
+		flex: 0 0 auto;
+		.img{
+			padding:0 0.843rem;
+		}
 		.text {
-			font-size:12px;
+			display:flex;
+			flex-direction:column;
+			justify-content:center;
+			.welcome {
+				font-size:12px;
+			}
+			.name{
+				font-size:14px;
+				font-weight:bold;
+			}
 		}
 	}
-	.brand {
+	.fill {
 		display: flex;
-		flex-direction:column;
-		.logo {
-			width:10rem;
-		}
-		.text{
-			font-size:14px;
-			font-weight:bold;
+		flex: 1 1 100%;
+		justify-content:center;
+		.brand {
+			display: flex;
+			flex-direction:column;
+			max-height:3.5rem;
+			flex:0 0 auto;
+			.logo {
+				width:10rem;
+			}
+			.text{
+				font-size:14px;
+				font-weight:bold;
+			}
 		}
 	}
-	
 `;
 
 const Header:React.FC = () => {
-	React.useEffect(() => {
-		console.log("test");
-	},[]);
 	return (
 		<StyledHeader>
 			<div className="profile">
 				<img className="img" src={profile} alt="profile"/>
-				<div>
-					<span className="text">Welcome</span>
+				<div className="text">
+					<span className="welcome">Welcome</span>
+					<span className="name">Dennis Smith</span>
 				</div>
 			</div>
-			<div className="brand">
-				<img className="logo" src={logo} alt="cassini"/>
-				<span className="text"> {localization.title}</span>
+			<div className="fill">
+				<div className="brand">
+					<img className="logo" src={logo} alt="cassini"/>
+					<span className="text"> {localization.title}</span>
+				</div>
 			</div>
+			
 		</StyledHeader>
 	);
 };
--- scripts/page/index.tsx
+++ scripts/page/index.tsx
@@ -7,17 +7,28 @@ import Header from "./header";
 import {init} from "./index.service";
 
 const Container = styled.div`
+	@media screen and (min-width: 1300px) {
+		display:flex;
+		flex:0 0 1300px;
+	}
 	display:flex;
-	flex-direction:column;
+	flex:1 1 100%;
+	flex-direction:row;
+	.content{
+		display:flex;
+		flex-direction:column;
+		flex:1 1 100%;
+	}
 `;
-
 const Page:React.FC = () => {
 	React.useEffect(init,[]);
 	return (
 		<Container>
-			<Header />
-			<div>
-				This is page
+			<div className="content">
+				<Header />
+				<div>
+					This is page
+				</div>
 			</div>
 		</Container>
 		
